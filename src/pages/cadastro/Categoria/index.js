fba2f5be2ac7b05ae2f9c88288b7839aa5985564
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-07-29 20:31:23 -0300

    Aula #3

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 import PageDefault from '../../PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+  const initialData = { nome: '', descricao: '', cor: '' };
+  const [categorias, setCategorias] = useState([]);
+  const [formData, setFormData] = useState(() => initialData);
+
+  function setValue(key, value) {
+    setFormData({ ...formData, [key]: value });
+  }
+
+  function handleChange(e) {
+    const { getAttribute, value } = e.target;
+    setValue(getAttribute('name'), value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCategorias([...categorias, formData]);
+    setFormData(initialData);
+  }
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type='text' />
-        </label>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          label='Nome da Categoria'
+          type='text'
+          name='nome'
+          value={formData.nome}
+          onChange={handleChange}
+        />
+        <FormField
+          label='Descrição'
+          type='textarea'
+          name='descricao'
+          value={formData.descricao}
+          onChange={handleChange}
+        />
+        <FormField
+          label='Cor'
+          type='color'
+          name='cor'
+          value={formData.cor}
+          onChange={handleChange}
+        />
         <button>Cadastrar</button>
       </form>
+      <ul>
+        {categorias.map((categoria, index) => (
+          <li key={`${categoria}${index}`}>{categoria.nome}</li>
+        ))}
+      </ul>
       <Link to='/'>Ir para home</Link>
     </PageDefault>
   );
