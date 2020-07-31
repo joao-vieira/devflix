a797cf661119fcd0d42d939cea0f0b7aad2b155c
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-07-30 22:16:52 -0300

    Update URLs

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -25,7 +25,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://rsflix.herokuapp.com/categorias';
     fetch(URL)
       .then(async (response) => {
         const categoriasResponse = await response.json();
